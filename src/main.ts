6a1377b07abb6072214b01c7c529798bc3e0eefd
daSugano <s_sugano@dreamarts.co.jp>
2022-09-20 17:44:42 +0900

    cors settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // TODO ホワイトリストは最終的なフロントエンドのデプロイ先urlを指定
+  app.enableCors({
+    credentials: true,
+    origin: [
+      "http://localhost:8080",
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
