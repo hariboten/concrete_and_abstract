20031fca0591258a06478870b6840fe6bc29f38c
daSugano <s_sugano@dreamarts.co.jp>
2022-09-22 17:16:09 +0900

    error fix and config rewrite

--- src/concrete-and-abstract/fake-answers-repository.ts
+++ src/concrete-and-abstract/fake-answers-repository.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import {Answer} from './answer';
-import {AnswerRepository} from './answer-repository';
+import { Answer } from './answer';
+import { AnswerRepository } from './answer-repository';
 
 @Injectable()
 export class FakeAnswersRepository implements AnswerRepository {
-	private readonly answers = new Map<string, Answer>;
+	private readonly answers = new Map<string, Answer>();
 
 
 	async getAnswer(answer: string): Promise<Answer> {
@@ -14,7 +14,7 @@ export class FakeAnswersRepository implements AnswerRepository {
 		return [...this.answers.values()];
 	}
 
-	async postAnswer(answer: Answer): Promise<Answer>{
+	async postAnswer(answer: Answer): Promise<Answer> {
 		if (!this.answers.has(answer.answer)) {
 			this.answers.set(answer.answer, answer);
 		}
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get<ConfigService>(ConfigService);
+  const config = new ConfigService();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // TODO ホワイトリストは最終的なフロントエンドのデプロイ先urlを指定
   app.enableCors({
