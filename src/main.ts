5f2fda724a87f2d4b13b8312b1aba0d17f1953b3
daSugano <s_sugano@dreamarts.co.jp>
2022-09-22 10:54:06 +0900

    updated cors whitelist, package.json, yarn.lock

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // TODO ホワイトリストは最終的なフロントエンドのデプロイ先urlを指定
   app.enableCors({
     credentials: true,
     origin: [
-      "http://localhost:8080",
+      config.get("CORS_LOCAL"),
+      config.get("CORS_DA_1"),
+      config.get("CORS_DA_2"),
+      config.get("CORS_PROD")
     ],
   });
   await app.listen(3000);
